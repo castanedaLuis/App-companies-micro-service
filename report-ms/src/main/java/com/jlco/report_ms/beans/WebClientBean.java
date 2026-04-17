82c7a6f36246b9e4009a2008256e911423600631
Jose Luis Castañeda <programador1996@outlook.es>
2026-04-17 12:49:55 -0600

    Add dependeci web client and configuration new bean WebClient

---
+++ report-ms/src/main/java/com/jlco/report_ms/beans/WebClientBean.java
@@ -0,0 +1,15 @@
+package com.jlco.report_ms.beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientBean {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
+}
+
