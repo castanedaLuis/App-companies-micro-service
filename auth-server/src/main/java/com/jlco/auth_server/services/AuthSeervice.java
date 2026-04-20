a051f6c4f2ac034baebc652cc899b87e157f6c11
Jose Luis Castañeda <programador1996@outlook.es>
2026-04-20 13:17:36 -0600

    Create DTOs, service and his implementation

---
+++ auth-server/src/main/java/com/jlco/auth_server/services/AuthSeervice.java
@@ -0,0 +1,10 @@
+package com.jlco.auth_server.services;
+
+import com.jlco.auth_server.dtos.TokenDto;
+import com.jlco.auth_server.dtos.UserDto;
+
+public interface AuthSeervice {
+
+    TokenDto login(UserDto user);
+    TokenDto validateToken(TokenDto token);
+}
