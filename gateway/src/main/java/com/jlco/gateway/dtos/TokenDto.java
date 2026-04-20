6cd9bdd79eba9c303de0edd145031076551d5340
Jose Luis Castañeda <programador1996@outlook.es>
2026-04-20 14:16:17 -0600

    Implements filters to have security with JWT and

---
+++ gateway/src/main/java/com/jlco/gateway/dtos/TokenDto.java
@@ -0,0 +1,14 @@
+package com.jlco.gateway.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TokenDto {
+
+    private String accessToken;
+}
